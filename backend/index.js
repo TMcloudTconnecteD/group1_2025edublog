31a4c30d50a6cd64b6f9b38a7591745f521e483d
TMcloudTconnecteD <larrygerald76@gmail.com>
2025-11-16 17:45:34 +0300

    try

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,9 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/upload", uploadRoutes);
+app.use("/auth", authRoutes);
+app.use("/posts", postRoutes);
+app.use("/upload", uploadRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
